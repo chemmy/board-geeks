53279a5131c193062188a8a2626d5b989e5dcbb7
Chemmy Tutor <chemmy0430@gmail.com>
2021-02-11 15:53:53 +0800

    bg-0002: Setup sass

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import './index.scss';
+import App from './scenes/app';
 
 ReactDOM.render(
   <React.StrictMode>
